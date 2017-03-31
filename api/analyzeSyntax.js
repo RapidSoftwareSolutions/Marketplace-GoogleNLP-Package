466387a8cbfe4356e61139e2bda39391699464c1
Dmitry Shumytskyi <d.shumytskyi@gmail.com>
2017-03-31 19:25:40 +0300

    Update analyzeSyntax.js

--- api/analyzeSyntax.js
+++ api/analyzeSyntax.js
@@ -8,7 +8,7 @@ module.exports = (req, res) => {
     req.body.args = lib.clearArgs(req.body.args);
 
     let { 
-        apiKey,
+        accessToken,
         encodingType,
         documentType='UTF8',
         documentLanguage,
@@ -16,7 +16,7 @@ module.exports = (req, res) => {
         documentGcsContentUri
     } = req.body.args;
         
-    let required = lib.parseReq({apiKey});
+    let required = lib.parseReq({accessToken});
 
     if(required.length > 0) 
         throw new RapidError('REQUIRED_FIELDS', required);
@@ -40,7 +40,7 @@ module.exports = (req, res) => {
         method: 'POST',
         body: JSON.stringify(params),
         headers: {
-            'Authorization': 'Bearer ' + apiKey
+            'Authorization': 'Bearer ' + accessToken
         }
     }, (err, response, reslut) => {
         if(!err && (/20.*/).test(response.statusCode))  
