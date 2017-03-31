3d3a3223d2a5c8aa9b8eab549cee7ce41f80afd3
Dmitry Shumytskyi <d.shumytskyi@gmail.com>
2017-03-31 18:37:20 +0300

    Update annotateText.js

--- api/annotateText.js
+++ api/annotateText.js
@@ -8,7 +8,7 @@ module.exports = (req, res) => {
     req.body.args = lib.clearArgs(req.body.args);
 
     let { 
-        accessToken,
+        apiKey,
         encodingType,
         documentType='UTF8',
         documentLanguage,
@@ -19,7 +19,7 @@ module.exports = (req, res) => {
         extractDocumentSentiment
     } = req.body.args;
         
-    let required = lib.parseReq({accessToken});
+    let required = lib.parseReq({apiKey});
 
     if(required.length > 0) 
         throw new RapidError('REQUIRED_FIELDS', required);
@@ -50,7 +50,7 @@ module.exports = (req, res) => {
         method: 'POST',
         body: JSON.stringify(params),
         headers: {
-            'Authorization': 'Bearer ' + accessToken
+            'Authorization': 'Bearer ' + apiKey
         }
     }, (err, response, reslut) => {
         if(!err && (/20.*/).test(response.statusCode))  
